11f21230899dce64b9dd8b576d57ad94323a4d96
50vincents <vincethai035@gmail.com>
2020-05-10 16:03:31 -0700

    Updates

---
+++ precache-manifest.d5e0cfe06f4b4720bb8bca8de3e45f61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5be144b9fbc97043ac9d7a0ebe2f59e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ece10f97fd7fa596d736",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "9435fa8f19ec96522379",
+    "url": "/robofriends/static/css/main.66d6fe88.chunk.css"
+  },
+  {
+    "revision": "ece10f97fd7fa596d736",
+    "url": "/robofriends/static/js/2.d1ffdf62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d1ffdf62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9435fa8f19ec96522379",
+    "url": "/robofriends/static/js/main.5e104894.chunk.js"
+  },
+  {
+    "revision": "3366afc3d2d4b771e11e",
+    "url": "/robofriends/static/js/runtime-main.d415c425.js"
+  }
+]);
